6fdd0207ef89dd0d3934fe2c49736b2b67277ff1
sh6khan <sadman.khan999@gmail.com>
2015-02-17 21:08:41 -0500

    fixing addition bug

--- lib/dbmapper.rb
+++ lib/dbmapper.rb
@@ -2,7 +2,7 @@ require "dbmapper/version"
 
 module Dbmapper
 	class DatabaseMapper
-		
+
 		def initlialize
 			Rails.application.eager_load!
 		end
@@ -11,7 +11,8 @@ module Dbmapper
 			#open file
 			text_file = File.open("mapped_databse.txt", 'w')
 			ActiveRecord::Base.descendants.each do |model|
-				text_file.write(model + '\n')
+				text_file.write(model)
+				text_file.write('\n')
 			end
 			puts "All your models have been written to mapped_databse.txt"
 			text_file.close
@@ -21,9 +22,11 @@ module Dbmapper
 			#open file
 			text_file = File.open("mapped_databse.txt", 'w')
 			ActiveRecord::Base.descendants.each do |model|
-				text_file.write(model + '\n');
+				text_file.write(model)
+				text_file.write('\n\n')
 				model.column_names.each do |column|
 					text_file.write(column)
+					text_file.write('\n')
 				end
 			end
 			puts "All your columns have been written to mapped_databse.txt"
@@ -35,11 +38,13 @@ module Dbmapper
 			text_file = File.open("mapped_databse.txt", 'w')
 			association_string = "-->"
 			ActiveRecord::Base.descendants.each do |model|
-				text_file.write(model + '\n')
+				text_file.write(model)
+				text_file.write('\n')
 				model.relfect_on_all_associations.map(&:name).each do |assocations|
 					association_string = association_string + " #{assocations},"
 				end 
-				text_file.write(association_string + '\n')
+				text_file.write(association_string)
+				text_file.write('\n\n')
 				association_string = "-->"
 			end
 			puts "all models with their assocations have been written to mapped_databse.txt"
